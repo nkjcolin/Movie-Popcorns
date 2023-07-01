eb4c57a37f3f4149f7cedeab0e9212fdbf22e41b
nkjcolin <49198636+nkjcolin@users.noreply.github.com>
2023-07-01 21:37:03 +0800

    update

--- movie/home/urls.py
+++ movie/home/urls.py
@@ -4,20 +4,20 @@ from . import views
 
 
 urlpatterns = [
-    path('homepage/', views.homepage, name='homepage'),                 # User presses to view homepage
-    path(r'movie/<int:titleID>', views.movie, name='movie'),            # User presses to view movie page       
-    path(r'movie/<str:title>', views.movieSearch, name='movieSearch'),  # User searches for a movie         
+    path('homepage/', views.homepage, name='homepage'),                                            # User presses to view homepage
+    path(r'movie/<int:titleID>', views.movie, name='movie'),                                       # User presses to view movie page       
+    path(r'movie/<str:title>', views.movieSearch, name='movieSearch'),                             # User searches for a movie         
 
-    path('movie/', views.account, name='movie'),                        # User presses to view movie page
-    path('actor/', views.account, name='actor'),                        # User presses to view actor page
-    path('account/', views.account, name='account'),                    # User presses to view account page
+    path('movie/', views.account, name='movie'),                                                   # User presses to view movie page
+    path('actor/', views.account, name='actor'),                                                   # User presses to view actor page
+    path('account/', views.account, name='account'),                                               # User presses to view account page
 
-    path('profile/', views.profile, name='profile'),                    # User presses to view profile page
-    path("logout/",views.logout_view,name='logout'),                    # user logout 
-    path('login/', views.login_view, name='login'),                     # User presses to view login page
-    path('register/', views.register, name='register'),                 # User presses to view registration page
+    path('profile/', views.profile, name='profile'),                                               # User presses to view profile page
+    path("logout/",views.logout_view,name='logout'),                                               # user logout 
+    path('login/', views.login_view, name='login'),                                                # User presses to view login page
+    path('register/', views.register, name='register'),                                            # User presses to view registration page
 
-    path('genre/', views.genre, name='genre'),                          # User presses to choose genre
+    path('genre/', views.genre, name='genre'),                                                     # User presses to choose genre
     path('genre/genreSelect/<str:genreselection>/', views.genreSelect, name='genreSelect'),        # display genre selected
 
     path('sorted-movies/', views.sorted_movies, name='sorted_movies'),
